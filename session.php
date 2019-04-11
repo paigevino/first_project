aa2675f8ef7fe9e02f461e9b656118133603f7b8
paigevino <49433905+paigevino@users.noreply.github.com>
2019-04-11 10:42:17 +0530

    Add files via upload

---
+++ session.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+?>
+<html>
+<form action="text.php" method="post">
+<input type="text" name="username"/><br>
+<input type="submit" name="submit" value="submit">
+</form>
+</html>
